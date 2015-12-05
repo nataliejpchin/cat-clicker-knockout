5c067ea9924f8556b62ea86cc3f590ab5efcd6ae
Natalie Chin <natalie.j.chin@gmail.com>
2015-12-05 12:39:00 -0600

    create a new custom constructor called Cat with data parameter and set the properties and then create a new Cat object using those properties

--- js/app.js
+++ js/app.js
@@ -1,17 +1,21 @@
 // model - all the cat logic
-var Cat = function(){
+// object literal called data
+// creating a new custom constructor called Cat with data parameter so we can set the properties
+var Cat = function(data){
   
   // number of clicks default at 0
-  this.clickCount = ko.observable(0);
+  this.clickCount = ko.observable(data.clickCount);
   
   // name of cat
-  this.name = ko.observable('Tabby');
+  this.name = ko.observable(data.name);
   
   // cat image
-  this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
+  this.imgSrc = ko.observable(data.imgSrc);
   
+  // list of nicknames
+  this.nicknames = ko.observableArray(data.nicknames);
   
-  
+  //catLevel does not use the data parameter because everycat will always use the same catLevel function
   // change cat level based on number of clicks
   this.catLevel = ko.computed(function() {
     
@@ -23,12 +27,7 @@ var Cat = function(){
       
   }, this);
   
-  // list of nicknames
-  this.nicknames = ko.observableArray([
-    { nickname: 'kitty' },
-    { nickname: 'blackie' },
-    { nickname: 'whity' }
-  ]);
+  
 }
 
 // octopus
@@ -36,7 +35,14 @@ var ViewModel = function(){
   
   // refer to ViewModel scope/binding context
   var self = this;
-  this.currentCat = ko.observable( new Cat() );
+  
+  // creating a new Cat object
+  this.currentCat = ko.observable( new Cat({
+    clickCount: 0,
+    name: 'Tabby',
+    imgSrc: 'img/434164568_fea0ad4013_z.jpg',
+    nicknames: ['kitty', 'blacktie', 'whity']
+  }) );
   
   //increase number of click by 1 each time the cat image is clicked. Do not need to call this.currentCat.clickcount anymore because already in the currentCat binding context because of the with in index.html
   this.incrementCounter = function(){
